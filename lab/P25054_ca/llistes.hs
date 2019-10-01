bac4c0c4042a1f7fdaf2b789b800b17a288a279b
guillempla <guillem.pla@est.fib.upc.edu>
2019-10-01 12:58:38 +0200

    Afegir llistes i ordre_superior

--- lab/P25054_ca/llistes.hs
+++ lab/P25054_ca/llistes.hs
@@ -2,6 +2,8 @@ myLength :: [Int] -> Int
 myLength [] = 0
 myLength (x:xs) = 1 + myLength xs
 
+
+
 myMaximum :: [Int] -> Int
 myMaximum [x] = x
 myMaximum (x:xs)
@@ -10,10 +12,85 @@ myMaximum (x:xs)
   where
     max = myMaximum xs
 
+
+
+intToFloat::Int -> Float
+intToFloat n = fromInteger (toInteger n)
+
 average :: [Int] -> Float
-average [x] = x
-average (x:xs) = div mySum [y] myLength [u]
+average [x] = intToFloat x
+average (x:xs) = intToFloat (x+sum xs) / intToFloat (1 + myLength xs)
   where
-    mySum :: [Int] -> Int
-    mySum [x] = x
-    mySum (x:xs) = x + mySum xs
+    sum :: [Int] -> Int
+    sum [] = 0
+    sum (x:xs) = x + sum xs
+
+
+
+buildPalindrome :: [Int] -> [Int]
+buildPalindrome [] = []
+buildPalindrome x = reverse x ++ x
+  where
+    reverse :: [Int] -> [Int]
+    reverse [x] = [x]
+    reverse (x:xs) = reverse xs ++ [x]
+
+
+
+inside :: [Int] -> Int -> Bool
+inside [y] x = y == x
+inside (y:ys) x = (y == x) || inside ys x
+
+remove :: [Int] -> [Int] -> [Int]
+remove [] [] = []
+remove [] y = []
+remove x [] = x
+remove (x:xs) y
+    | not (inside y x)  = (x:(remove xs y))
+    | otherwise         = remove xs y
+
+
+
+flatten :: [[Int]] -> [Int]
+flatten [] = []
+flatten (x:xs) = x ++ flatten xs
+
+
+
+oddList :: [Int] -> [Int]
+oddList [] = []
+oddList (x:xs)
+  | (mod x 2) == 0  = (x:(oddList xs))
+  | otherwise       = oddList xs
+
+evenList :: [Int] -> [Int]
+evenList [] = []
+evenList (x:xs)
+  | (mod x 2) == 0  = evenList xs
+  | otherwise       = (x:(evenList xs))
+
+oddNevens :: [Int] -> ([Int],[Int])
+oddNevens [] = ([],[])
+oddNevens x = ((evenList x), (oddList x))
+
+
+
+-- isPrime :: Int -> Bool
+-- isPrime 2 = True
+-- isPrime n = isPrime' 2
+--   where
+--     isPrime' :: Int -> Bool
+--     isPrime' d
+--       | mod n d == 0    = False
+--       | d == (div n 2)  = True
+--       | otherwise       = isPrime' (d + 1)
+--
+-- primeDivisors :: Int -> [Int]
+-- primeDivisors 1 = []
+-- primeDivisors n = primeDivisors' 2
+--   where
+--     primeDivisors' :: Int -> [Int]
+--     primeDivisors' d
+--       | d > n = []
+--       | ((mod n d) == 0) && isPrime d = (d:(primeDivisors' (d+1)))
+--       | otherwise                     = primeDivisors (d+1)
---
+++ lab/P93632_ca/ordre_superior.hs
@@ -0,0 +1,14 @@
+eql :: [Int] -> [Int] -> Bool
+eql x y = length x == length y && and (zipWith (==) x y)
+
+prod :: [Int] -> Int
+prod x = foldr (*) 1 x
+
+prodOfEvens :: [Int] -> Int
+prodOfEvens x = foldr (*) 1 (filter even x)
+
+powersOf2 :: [Int]
+powersOf2 = iterate (*2) 1
+
+-- scalarProduct :: [Float] -> [Float] -> Float
+-- scalarProduct x y =
