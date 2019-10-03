bc0ef3dda2d57d50f86d8a1760a97c0b18a97d91
guillempla <guillem.pla@est.fib.upc.edu>
2019-10-03 11:47:42 +0200

    Afegir tot

--- lab/lab2/P93632_ca/ordre_superior.hs
+++ lab/lab2/P93632_ca/ordre_superior.hs
@@ -10,5 +10,5 @@ prodOfEvens x = foldr (*) 1 (filter even x)
 powersOf2 :: [Int]
 powersOf2 = iterate (*2) 1
 
--- scalarProduct :: [Float] -> [Float] -> Float
--- scalarProduct x y =
+scalarProduct :: [Float] -> [Float] -> Float
+scalarProduct x y = foldr (+) 0 (zipWith (*) x y)
--- lab/lab3/P98957_ca/llistes_infinites.hs
+++ lab/lab3/P98957_ca/llistes_infinites.hs
@@ -22,3 +22,35 @@ factorials = scanl (*) 1 [1..]
 
 fibs :: [Integer]
 fibs = 0 : 1 : (zipWith (+) fibs (tail fibs))
+
+
+isPrime :: Integer -> Bool
+isPrime 0 = False
+isPrime 1 = False
+isPrime 2 = True
+isPrime n = isPrime' 2
+  where
+    isPrime' :: Integer -> Bool
+    isPrime' d
+      | mod n d == 0    = False
+      | d == (div n 2)  = True
+      | otherwise       = isPrime' (d + 1)
+
+primes :: [Integer]
+primes = filter isPrime nats
+
+isHamming :: Integer -> Bool
+isHamming 0 = False
+isHamming 1 = True
+isHamming 2 = True
+isHamming 3 = True
+isHamming 5 = True
+isHamming n = isHamming' n
+  where
+    isHamming' :: Integer -> Bool
+    isHamming' n
+      | mod n 2 == 0 || mod n 3 == 0 || mod n 5 == 0 = True
+      | otherwise = False
+
+hammings:: [Integer]
+hammings = filter isHamming nats
