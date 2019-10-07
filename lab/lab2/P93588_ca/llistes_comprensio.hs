c23f98605c2c747294fb807393c7f80644a6d666
guillempla <guillem.pla@est.fib.upc.edu>
2019-10-07 13:18:34 +0200

    Part del problema llistes_comprensio

--- lab/lab2/P93588_ca/llistes_comprensio.hs
+++ lab/lab2/P93588_ca/llistes_comprensio.hs
@@ -1,1 +1,8 @@
-myMap :: (a - > b) -> [a] -> [b]
+myMap :: (a -> b) -> [a] -> [b]
+myMap f xs = [f x | x <- xs]
+
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter f xs = [x | x <- xs, f x]
+
+myZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+myZipWith f xs ys = [f x y | (x,y) <- zip xs ys]
