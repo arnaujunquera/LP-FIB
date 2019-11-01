ed2e168950ab832f5afafba11aed1d1d3a03d780
guillempla <guillem.pla@est.fib.upc.edu>
2019-11-01 16:17:05 +0100

    Expressions no acabat

---
+++ lab5/P70540_expressions/expressions.hs
@@ -0,0 +1,17 @@
+data Expr = Val Int | Add Expr Expr | Sub Expr Expr | Mul Expr Expr | Div Expr Expr
+
+eval1 :: Expr -> Int
+eval1 (Val x) = x
+eval1 (Add x y) = (eval1 x) + (eval1 y)
+eval1 (Sub x y) = (eval1 x) - (eval1 y)
+eval1 (Mul x y) = (eval1 x) * (eval1 y)
+eval1 (Div x y) = div (eval1 x) (eval1 y)
+
+eval2 :: Expr -> Maybe Int
+eval2 (Val x) = Just x
+eval2 (Add x y) = 
+-- eval2 (Sub x y) =
+-- eval2 (Mul x y) =
+-- eval2 (Div x y)
+--   |  = Nothing
+--   | otherwise =
