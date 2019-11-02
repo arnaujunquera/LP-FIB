52a45c0a0fe9d0eb7dbc0b6a0c5f537ed64b9862
guillempla <guillem.pla@est.fib.upc.edu>
2019-11-02 11:57:18 +0100

    Ordre superior 2 acabat

--- lab2/P31745_ordre_superior_2/ordre_superior_2.hs
+++ lab2/P31745_ordre_superior_2/ordre_superior_2.hs
@@ -3,8 +3,10 @@ flatten = foldr (++) []
 
 myLength :: String -> Int
 myLength [] = 0
-myLength x = last $ zipWith (f) x (iterate (+1) 1)
-    where f a b = b
+myLength x = last $ zipWith (\a b -> b) x (iterate (+1) 1)
+
+myReverse :: [Int] -> [Int]
+myReverse a = foldr (\x y -> y++[x]) [] a
 
 countIn :: [[Int]] -> Int -> [Int]
 countIn l x = map (f x) l
