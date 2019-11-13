a72055fc8a1cb68777a3eb026821c0769ec81e57
guillempla <guillem.pla@est.fib.upc.edu>
2019-11-13 09:46:31 +0100

    Mig fet

---
+++ python/P80049_us_iterables/us_iterables.py
@@ -0,0 +1,13 @@
+def count_unique(L):
+    return len(set(L))
+
+def remove_duplicates(L):
+    return set(L)
+
+def flatten(L):
+    return 
+
+print(count_unique([1, 3, 2, 2, 3, 4]))
+print(remove_duplicates([3, 1, 3, 2, 3, 2, 3, 4]))
+# print(flatten([[2, 6], [8, 1, 4], [], [1]]))
+# print(flatten_rec([3, [1], [], [4, [], [5, 3]], [2, 1]]))
