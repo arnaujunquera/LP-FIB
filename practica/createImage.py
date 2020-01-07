df1290b92cb018402bbc4a9e590a2b2773950548
guillempla <guillem.pla@est.fib.upc.edu>
2020-01-07 10:16:58 +0100

    Added arrows and colors

--- practica/createImage.py
+++ practica/createImage.py
@@ -1,13 +1,20 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-
+from collections import defaultdict
+G=nx.DiGraph()
+print(type(G))
 G=nx.read_gpickle("graph.gpickle")
+print(type(G))
 
 pos = nx.circular_layout(G)
-nx.draw(G, pos, labels={node:node for node in G.nodes()})
+
+colors = nx.get_edge_attributes(G, 'color')
+edge_colors = [colors[edge] for edge in G.edges()]
+
+nx.draw(G, pos, node_size = 600, edge_color = edge_colors, labels={node:node for node in G.nodes()})
 
 edge_labels = nx.get_edge_attributes(G,'label')
-nx.draw_networkx_edge_labels(G, pos, arrows = True, edge_labels = edge_labels)
+nx.draw_networkx_edge_labels(G, pos, edge_labels = edge_labels)
 
 plt.savefig("image_graph.png", format="PNG")
 plt.show()
